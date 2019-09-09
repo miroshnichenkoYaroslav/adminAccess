ebda5c8c925db1c31b240e2fca37ac1583963cd4
mirosnicenkoo19 <miroshnichenkoo19@gmail.com>
2019-09-09 18:11:22 +0300

    Added output of the table with users in the admin panel

---
+++ resources/js/components/UsersTableComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <datatable :columns="columns" :data="rows"></datatable>
+</template>
+
+<script>
+import Vue from 'vue';
+import DatatableFactory from 'vuejs-datatable';
+
+Vue.use(DatatableFactory);
+    export default {
+        mounted() {
+            // TODO overlay
+            axios.post('/users')
+                .then((response) => {
+                    console.log(response);
+                    this.rows = response.data;
+                })
+        },
+        data() {
+            return {
+                columns: [
+                    {label: 'id', field: 'id'},
+                    {label: 'Role', field: 'role', headerClass: 'class-in-header second-class'},
+                    {label: 'First Name', field: 'name'},
+                    {label: 'Email', field: 'email'},
+                ],
+                rows: [],
+            };
+        },
+    }
+</script>
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div>
+        <users-table-component></users-table-component>
+    </div>
+@endsection
+
+@push('scripts')
+    <script>
+    </script>
+@endpush
